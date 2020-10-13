abd0225d6a8b017df73e4a0d0f9805c1043a3931
Sarah ZIANE <sarahziane5@gmail.com>
2020-10-13 19:58:00 +0300

    homework done

--- DataS_3/Program.cs
+++ DataS_3/Program.cs
@@ -46,26 +46,26 @@ namespace DataS_3
 
         }
 
-        static void Task2()
-        {
-            Console.WriteLine("Write a sentence : ");
-            string sentence = Console.ReadLine();
-            string[] words = sentence.Split(" ");
+        //static void Task2()
+        //{
+        //    Console.WriteLine("Write a sentence : ");
+        //    string sentence = Console.ReadLine();
+        //    string[] words = sentence.Split(" ");
 
-            string longestword = "";
+        //    string longestword = "";
 
-            foreach (string word in words)
-            {
-                if(longestword.Length < CleanWord(word).Length)
-                {
-                    longestword = word;
-                }
-            }
+        //    foreach (string word in words)
+        //    {
+        //        if(longestword.Length < CleanWord(word).Length)
+        //        {
+        //            longestword = word;
+        //        }
+        //    }
 
 
-            Console.WriteLine($"The longest word is : {longestword}");
+        //    Console.WriteLine($"The longest word is : {longestword}");
 
-        }
+        //}
 
         static void Task3()
         {
@@ -87,8 +87,105 @@ namespace DataS_3
          
         }
 
+        static void Array()
+        {
+          // bool result = false;
+            ArrayList array = new ArrayList();
+            for (int i = 0; i <= 10; i++) 
+            {
+                array.Add(i);
+            }
+            foreach (int item in array)
+            {
+                Console.WriteLine(item +"\n");
+            }
+            Console.WriteLine("Write a number to check if it's in the array : ");
+            int check = int.Parse(Console.ReadLine());
+            if (array.Contains(check) == true)
+            {
+                Console.WriteLine("\nThe number is in the array");
+            }
+            else
+                Console.WriteLine("\nThe number is not in the array");
 
+            //foreach (int item in array)
+            //{
+            //    if (check == item)
+            //    {
+            //        result = true;
+            //    }
+            //    else
+            //    {
+            //        result = false;
+            //    }
+            //}
+            //Console.WriteLine($"The number is in the array : {result}"); 
+            /////////// fonctionne pas jcp pk /////////////////
+        }
 
+        static void Stack()
+        {
+            Stack<int> mystack = new Stack<int>();
+            for (int i = 0; i <=10; i++)
+            {
+                mystack.Push(i);
+            }
+            foreach(int item in mystack)
+            {
+                Console.WriteLine($"\n{item}");
+            }
+            Console.WriteLine("Write a number to check if it's in the stack : ");
+            int check = int.Parse(Console.ReadLine());
+            if (mystack.Contains(check) == true)
+            {
+                Console.WriteLine("\nThe number is in the stack");
+            }
+            else
+                Console.WriteLine("\nThe number is not in the stack");
+
+        }
+        static void Queues()
+        {
+            Queue<int> myqueue = new Queue<int>();
+            for (int i = 0; i <= 10; i++)
+            {
+                myqueue.Enqueue(i);
+            }
+            foreach (int item in myqueue)
+            {
+                Console.WriteLine($"\n{item}");
+            }
+            Console.WriteLine("Write a number to check if it's in the queue : ");
+            int check = int.Parse(Console.ReadLine());
+            if (myqueue.Contains(check) == true)
+            {
+                Console.WriteLine("\nThe number is in the queue");
+            }
+            else
+                Console.WriteLine("\nThe number is not in the queue");
+
+        }
+        static void List()
+        {
+            List<int> mylist = new List<int>();
+            for (int i = 0; i <= 10; i++)
+            {
+                mylist.Add(i);
+            }
+            foreach (int item in mylist)
+            {
+                Console.WriteLine($"\n{item}");
+            }
+            Console.WriteLine("Write a number to check if it's in the list : ");
+            int check = int.Parse(Console.ReadLine());
+            if (mylist.Contains(check) == true)
+            {
+                Console.WriteLine("\nThe number is in the list");
+            }
+            else
+                Console.WriteLine("\nThe number is not in the list");
+
+        }
 
         static void Main(string[] args)
         {
@@ -99,7 +196,14 @@ namespace DataS_3
                 Console.WriteLine("\n\n Task 1 : \n\n"
                                      + " 1 : Names and birth dates (arrays)"
 
-                                     + "\n\n Task 2 : Longest word in a sentence"
+                                     + "\nTask 2 : Longest word in a sentence"
+                                     + "\n Task 3 : Reverse the words of a sentence"
+
+                        + "\n\nHomework : "
+                  + "\n\n 4 : Array"
+                   + "\n\n 5 : Stack"
+                    + "\n\n 6 : Queues"
+                     + "\n\n 7 : List<T>"
 
                                      + "\n\n Please choose : ");
                 int exo = Convert.ToInt32(Console.ReadLine());
@@ -111,11 +215,23 @@ namespace DataS_3
                         break;
 
                     case 2:
-                        Task2();
+                    //    Task2();
                         break;
 
                     case 3:
                         Task3();
+                        break;
+                    case 4:
+                        Array();
+                        break;
+                    case 5:
+                        Stack();
+                        break;
+                    case 6:
+                        Queues();
+                        break;
+                    case 7:
+                        List();
                         break;
                 }
                 Console.WriteLine("\nPress any key to exit... ");
